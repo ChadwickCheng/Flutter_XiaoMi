076072b256dbe981e27c5d1a40e0c051ccb06bce
Dan Cheng <ChadwickCheng@outlook.com>
2024-05-03 17:41:49 +0800

    首页轮播

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,14 +1,26 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomeController extends GetxController {
+  Dio dio = new Dio();
+
+  // 浮动导航开关
   RxBool flag = false.obs;
   // 说白了就是通过监听listview的滚动举例来改变flag，通过flag的三目运算来改变appbar的样式，所以会见到大量的有关flag的三目运算
   final ScrollController scrollController = ScrollController();
+  // 轮播图
+  RxList swiperList = [].obs;
 
   @override
   void onInit() {
     super.onInit();
+    scrollControllerListener();
+    // 请求轮播图数据
+    getFocusData();
+  }
+
+  void scrollControllerListener(){
     scrollController.addListener(() {
       if (scrollController.position.pixels > 10) {
         if (flag.value == false) {
@@ -27,8 +39,10 @@ class HomeController extends GetxController {
     });
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  getFocusData() async{
+    var res = await dio.get('https://xiaomi.itying.com/api/focus');
+    print(res.data);
+    swiperList.value = res.data['result'];
+    update();
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,95 +1,111 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/home_controller.dart';
-
 import '../../../services/keepAliveWrapper.dart';
 import '../../../services/screenAdapter.dart';
 import '../../../services/ityingFonts.dart';
+import 'package:flutter_swiper_view/flutter_swiper_view.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
+
+  // 顶部导航
+  Widget _appBar(){
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Obx(() => AppBar(
+        leading: controller.flag.value?const Text(""):const Icon(
+          ItyingFonts.xiaomi,
+          color: Colors.white,
+        ),
+        leadingWidth:controller.flag.value?ScreenAdapter.width(40):ScreenAdapter.width(140) ,
+        title: AnimatedContainer(
+          width: controller.flag.value?ScreenAdapter.width(800):ScreenAdapter.width(620),
+          height: ScreenAdapter.height(96),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(230, 252, 243, 236),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          duration: const Duration(milliseconds: 600),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(ScreenAdapter.width(34), 0, ScreenAdapter.width(10), 0),
+                child: const Icon(Icons.search),
+              ),
+              Text(
+                "手机",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: ScreenAdapter.fontSize(32)
+                )
+              )
+            ],
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: controller.flag.value?Colors.white:Colors.transparent,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(Icons.qr_code,color:controller.flag.value?Colors.black87:Colors.white,)
+          ),
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(Icons.message,color:controller.flag.value?Colors.black87:Colors.white)
+          )
+        ],
+      )),
+    );
+  }
+
+  // 内容区域
+  Widget _homePage(){
+    return Positioned(
+      top: -40,
+      left: 0,
+      right:0,
+      bottom: 0,
+      child: ListView(
+        controller: controller.scrollController,
+        children: [
+          SizedBox(
+            width: ScreenAdapter.width(1080),
+            height: ScreenAdapter.height(682),
+            child: Obx(()=>Swiper(
+              itemCount: controller.swiperList.length,
+              autoplay: true,
+              itemBuilder: (BuildContext context, int index) {
+                String picUrl = "https://xiaomi.itying.com/${controller.swiperList[index]["pic"]}";
+                print('picUrl: $picUrl');
+                return Image.network(
+                  picUrl.replaceAll('\\', '/'),
+                  fit: BoxFit.fill,
+                );
+              },
+              pagination: const SwiperPagination(
+                builder: SwiperPagination.rect, // 分页指示器样式
+              ),
+              loop: true,
+            )),
+          )
+        ],
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return KeepAliveWrapper(
       child: Scaffold(
         body: Stack(
           children: [
-            Positioned(
-              top: -40,
-              left: 0,
-              right:0,
-              bottom: 0,
-              child: ListView.builder(
-                controller: controller.scrollController, //绑定controller
-                itemCount: 20,
-                itemBuilder: (context, index) {
-                  if (index == 0) {
-                    return SizedBox(
-                      width: ScreenAdapter.width(1080),
-                      height: ScreenAdapter.height(682),
-                      child: Image.network(
-                        "https://www.itying.com/images/focus/focus02.png",
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  } else {
-                    return ListTile(title: Text("第$index个列表"));
-                  }
-                }
-              )
-            ),
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: Obx(() => AppBar(
-                leading: controller.flag.value?const Text(""):const Icon(
-                  ItyingFonts.xiaomi,
-                  color: Colors.white,
-                ),
-                leadingWidth:controller.flag.value?ScreenAdapter.width(40):ScreenAdapter.width(140) ,
-                title: AnimatedContainer(
-                  width: controller.flag.value?ScreenAdapter.width(800):ScreenAdapter.width(620),
-                  height: ScreenAdapter.height(96),
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(230, 252, 243, 236),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  duration: const Duration(milliseconds: 600),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(ScreenAdapter.width(34), 0, ScreenAdapter.width(10), 0),
-                        child: const Icon(Icons.search),
-                      ),
-                      Text(
-                        "手机",
-                        style: TextStyle(
-                          color: Colors.black54,
-                          fontSize: ScreenAdapter.fontSize(32)
-                        )
-                      )
-                    ],
-                  ),
-                ),
-                centerTitle: true,
-                backgroundColor: controller.flag.value?Colors.white:Colors.transparent,
-                elevation: 0,
-                actions: [
-                  IconButton(
-                    onPressed: () {}, 
-                    icon: Icon(Icons.qr_code,color:controller.flag.value?Colors.black87:Colors.white,)
-                  ),
-                  IconButton(
-                    onPressed: () {}, 
-                    icon: Icon(Icons.message,color:controller.flag.value?Colors.black87:Colors.white)
-                  )
-                ],
-              )),
-            )
+            _homePage(),
+            _appBar(),
           ],
         ),
       )
