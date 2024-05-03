5e0caefd9603611dc0792a1306494ed5a2736df7
Dan Cheng <ChadwickCheng@outlook.com>
2024-05-03 16:50:17 +0800

    appbar

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,34 @@
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  RxBool flag = false.obs;
+  // 说白了就是通过监听listview的滚动举例来改变flag，通过flag的三目运算来改变appbar的样式，所以会见到大量的有关flag的三目运算
+  final ScrollController scrollController = ScrollController();
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels > 10) {
+        if (flag.value == false) {
+          // print("position.pixels > 10");
+          flag.value = true;
+          update();
+        }
+      }
+      if (scrollController.position.pixels < 10) {
+        if (flag.value == true) {
+            // print("position.pixels < 10");
+          flag.value = false;
+          update();
+        }
+      }
+    });
   }
 
   @override
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,12 @@ import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  //配置透明的状态栏 让手机最顶部的状态栏透明，和app的背景色一样
+  SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+  SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
   runApp(
     ScreenUtilInit(
       designSize: const Size(1080, 2400),   //设计稿的宽度和高度 px
@@ -13,6 +17,7 @@ void main() {
       splitScreenMode: true,
       builder: (context , child) {
         return  GetMaterialApp(
+          debugShowCheckedModeBanner: false,
           title: "Application",
           initialRoute: AppPages.INITIAL,
           getPages: AppPages.routes,
